a9b8e1dd5f5e4e2a2649dac7287e3814ea524bd6
Allard Schuurmans <a.schuurmans@4DotNet.nl>
2024-09-07 16:17:29 +0200

    Add project files.

---
+++ WixBootstrapperWpfApp1FrameWork/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+using System.Windows;
+
+using System.Windows.Interop;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private MainViewModel viewModel;
+
+    public MainWindow(ManagedBA bootstrapper)
+    {
+        InitializeComponent();
+
+        var vindowHandle = new WindowInteropHelper(this).EnsureHandle();
+
+        DataContext =
+            viewModel = new MainViewModel(bootstrapper) { ViewHandle = vindowHandle };
+    }
+
+    private void Install_Click(object sender, RoutedEventArgs e)
+    {
+        viewModel.InstallExecute();
+    }
+
+    private void Uninstall_Click(object sender, RoutedEventArgs e)
+    {
+        viewModel.UninstallExecute();
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void TextBlock_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        viewModel.ShowLog();
+    }
+}
---
+++ WixBootstrapperWpfApp1FrameWork/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WixSharp;
+using WixSharp.Bootstrapper;
+
+namespace WixBootstrapperWpfApp1FrameWork
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            string productMsi = BuildMsi();
+
+            var bootstrapper =
+                new Bundle("MyProduct",
+                    new MsiPackage(productMsi)
+                    {
+                        Id = "MyProductPackageId",
+                        DisplayInternalUI = true
+                    });
+
+            bootstrapper.Version = new Version("1.0.0.0");
+            bootstrapper.UpgradeCode = new Guid("0779a1bc-b66e-4815-93df-8644d4a928d9");
+
+            bootstrapper.Application = new ManagedBootstrapperApplication("%this%");
+
+            bootstrapper.Build("MyProduct.exe");
+        }
+
+        private static string BuildMsi()
+        {
+            var project = new Project("MyProduct",
+                new Dir(@"%ProgramFiles%\My Company\My Product",
+                    new File(@"..\..\Program.cs")));
+
+            project.GUID = new Guid("0779a1bc-b66e-4815-93df-8644d4a928d9");
+
+            return project.BuildMsi();
+        }
+    }
+}
